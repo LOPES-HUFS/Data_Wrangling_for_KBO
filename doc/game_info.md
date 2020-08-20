0752f3d930935e21070b8662aa05f9be412ed120
Jaehwan <dialektike@gmail.com>
2020-08-20 17:39:43 +0900

    코드 정리 및 설명서 시작

---
+++ doc/game_info.md
@@ -0,0 +1,26 @@
+# 게임 정보 가져오기
+
+현재 스트크립트가 돌아가서 자료를 가지고 오려면, `"20200801","HHLG0"`과 같은 형식의 자료가 필요하다. 이 자료를 이용해서 `main.get_data()`를 이용해서 경기 데이터를 불러온다. 다음 예를 봅시다.
+
+```python
+HHLG0_20200801 = `get_data("20200801","HHLG0")
+```
+
+이때 "20200801"은 경기 날짜, "HHLG0"은 한화, 엘지 1번째 경기를 뜻합니다. 파이썬에서 0부터 숫자를 세기 때문에 0이 첫번째 경기입니다.
+
+따라서 모든 경기를 불러오기 위해서는 앞에서 소개한 형식의 자료가 필요합니다. 현재 이 레포지토리에는 [KBO_gameid_full.csv](https://github.com/LOPES-HUFS/Data_Wrangling_for_KBO/blob/master/data/KBO_gameid_full.csv)에 2020년 8월까지의 자료가 들어 있습니다. 이를 이용하시면, 손쉽게 자료를 다운 받으실 수 있습니다.
+
+이 파일에 없는 게임 자료를 받으려면, 예를 들어 2020년 9월 자료를 받으려면, 위에서 말한 자료 목록을 만들어어야 합니다. 이 자료를 만드는 함수가 `game_info.get_data()`입니다. 이 함수의 용법은 다음과 같습니다.
+
+```python
+import game_info
+may_data = game_info.get_data(2020,"05","정규")
+game_id_may = game_info.modify_data(2020, may_data)
+```
+
+```python
+game_ids_df = pd.concat([game_id_may,game_id_june,game_id_july])
+
+game_ids_df.to_csv("kbo_2020_gameid_list.csv")
+
+```
--- game_info.py
+++ game_info.py
@@ -10,7 +10,9 @@ config.read('config.ini')
 Game_info_URL = config['DEFAULT']['Game_info_URL']
 chromium_location = config['DEFAULT']['chromium_location']
 
-def get_table(year,month,season):
+team_list = {'HT':'KIA','OB':'두산','LT':'롯데','NC':'NC','SK':'SK','LG':'LG','WO':'키움','HH':'한화','SS':'삼성','KT':'KT'}
+
+def get_data(year,month,season):
     ''' 
         Args: year,month,season
         year: 원하는 게임의 연도
@@ -26,12 +28,12 @@ def get_table(year,month,season):
         season = "0,9"
     else:
         season = "3,4"
+
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
     url = Game_info_URL + season
-    #url = f"https://www.koreabaseball.com/Schedule/Schedule.aspx?seriesId={season}" # 시범=1 정규=0,9 포스트=3,4,5,7
     driver = webdriver.Chrome(chromium_location, chrome_options=options)
     driver.get(url)
     time.sleep(1)
@@ -42,26 +44,27 @@ def get_table(year,month,season):
     table = driver.find_element_by_css_selector("table")
     gamelist=[]
     for row in table.find_elements_by_css_selector('tr'):
-        gamelist.append([d.text for d in row.find_elements_by_css_selector('td')])
+        gamelist.append([ \
+            d.text for d in row.find_elements_by_css_selector('td')])
         
     del gamelist[0]
     
-    [gamelist[i].insert(0,gamelist[i-1][0]) for i in range(0,len(gamelist)) if len(gamelist[i])==8]
+    [gamelist[i].insert(0, gamelist[i-1][0]) \
+        for i in range(0, len(gamelist)) if len(gamelist[i])==8]
     
-    gamelist_df=pd.DataFrame(gamelist,columns=['날짜','시간','경기','게임센터','하이라이트','TV','라디오','구장','비고'])
+    gamelist_df = pd.DataFrame( gamelist, \
+        columns = ['날짜','시간','경기','게임센터','하이라이트','TV','라디오','구장','비고'])
     driver.close()
     return gamelist_df
-
-team_list={'HT':'KIA','OB':'두산','LT':'롯데','NC':'NC','SK':'SK','LG':'LG','WO':'키움','HH':'한화','SS':'삼성','KT':'KT'}
 
 def change_team_name(string):
     temp = [string.find(team) for team in team_list.values()]
     temp[:] = [0 if ele != -1 else ele for ele in temp]
-    temp=temp.index(0)
-    temp=list(team_list.items())[temp]
+    temp = temp.index(0)
+    temp = list(team_list.items())[temp]
     return temp[0]
 
-def change_df(data):
+def modify_data(year, data):
     '''
     입력값: data는 get_game_info_table 함수의 return 값
 
@@ -80,9 +83,12 @@ def change_df(data):
     homecode=[change_team_name(i) for i in hometeam]
     awaycode=[change_team_name(i) for i in awayteam]
 
-    date=["2020"+i.split(".")[0]+"".join(re.findall("[0-9]",i.split(".")[1])) for i in data['날짜']]
-    gameid = [awaycode[i]+homecode[i]+"0" for i in range(0,len(awaycode))]
-    gameid_list = pd.DataFrame({"date":date,"away":awaycode,"home":homecode,"gameid":gameid})
+    date = [str(year) + i.split(".")[0] + \
+        "".join(re.findall("[0-9]",i.split(".")[1])) for i in data['날짜']]
+    
+    gameid = [awaycode[i] + homecode[i] + "0" for i in range(0,len(awaycode))]
+    gameid_list = pd.DataFrame( \
+            {"date": date, "away":awaycode,"home":homecode,"gameid":gameid})
 
     double_header = gameid_list.index[gameid_list.duplicated(keep=False)==True]
     
@@ -90,20 +96,6 @@ def change_df(data):
     gameid_list.gameid[double_header[one]] = [str(i).replace("0","2") for i in gameid_list.gameid[double_header[one]]]
     gameid_list.gameid[double_header[[x for x in range(0,len(double_header)) if x not in one]]] = [str(i).replace("0","1") for i in gameid_list.gameid[double_header[[x for x in range(0,len(double_header)) if x not in one]]]]
 
-    gameid_list = gameid_list[gameid_list.gameid!="드림나눔0"]
+    gameid_list = gameid_list[gameid_list.gameid != "드림나눔0"]
 
     return gameid_list
-
-
-
-#may_data = get_game_info_table(2020,"05","정규")
-#june_data = get_game_info_table(2020,"06","정규")
-#july_data = get_game_info_table(2020,"07","정규")
-
-#game_id_may = get_game_list(may_data)
-#game_id_june = get_game_list(june_data)
-#game_id_july = get_game_list(july_data)
-
-#game_ids_df = pd.concat([game_id_may,game_id_june,game_id_july])
-
-#game_ids_df.to_csv("kbo_2020_gameid_list.csv")
